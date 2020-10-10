129566b307547f794707c9414859d6fd66c0bb79
jasonantonucci <jasonantonucci@gmail.com>
2020-10-10 01:10:56 -0400

    phase 2 done

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ database.create_table()
 def prompt_add_movie():
 	title = input("Movie title: ")
 	release_date = input("Release date (dd-mm-YYYY): ")
+	parsed_date = datetime.datetime.strptime(release_date, "%d-%m-%Y")
 	timestamp = parsed_date.timestamp()
 	
 	database.add_movie(title, timestamp)
@@ -28,14 +29,21 @@ def print_movie_list(heading, movies):
 	print(f"-- {heading} movies --")
 	for movie in movies:
 		movie_date = datetime.datetime.fromtimestamp(movie[1])
-		human_date = movie_date.strftime("%b %d %Y")
+		human_date = movie_date.strftime("%d %b %Y")
 		print(f"{movie[0]} (on {human_date})")
 	print("---- \n")
 	
+
+def print_watched_movie_list(username, movies):
+	print(f"-- {username}'s watched movies --")
+	for movie in movies:
+		print(f"{movie[1]}")
+	print("---- \n")	
 	
 def prompt_watch_movie():
+	username = input("Username: ")
 	movie_title = input("Enter movie title you've watched: ")
-	database.watch_movie(movie_title)
+	database.watch_movie(username, movie_title)
 
 while (user_input := input(menu)) != "6":
 	if user_input == "1":
@@ -49,6 +57,24 @@ while (user_input := input(menu)) != "6":
 	elif user_input == "4":
 		prompt_watch_movie()
 	elif user_input == "5":
-		pass
+		 username = input("Username: ")
+		 movies = database.get_watched_movies(username)
+		 print_movie_list(username, movies)
 	else:
 		print("Invalid input, please try again!")
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
